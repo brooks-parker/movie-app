a68b87673bac0d22e1ed5fc4cdf8836d0d32834e
John <johntbrooks92@gmail.com>
2020-11-24 13:35:20 -0600

    first push my branch

--- js/movie-app.js
+++ js/movie-app.js
@@ -1,1 +1,13 @@
 "use strict";
+// filter by title, genre etc.
+// Enter a movie title and search
+//Title
+//Review Rating
+//Delete option for movies
+
+
+
+
+
+const url = "https://nine-shared-text.glitch.me/movies"
+console.log(url);
